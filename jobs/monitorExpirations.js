ff053816b2d9f8b786fae10ee9c91ecbd4308099
Mike Zarra <mkzarra@Mikes-Air.fios-router.home>
2019-10-04 14:25:59 -0400

    bugfix

--- jobs/monitorExpirations.js
+++ jobs/monitorExpirations.js
@@ -22,7 +22,7 @@ const monitorExpiration = cron.schedule('* * * * Friday', function() {
     for (let user of data) {
       if (user.pantries.length > 0) {
         const mailer = new Mailer(user, user.pantries.map(item => pantryTemplate(item)));
-        mailer.send();
+        // mailer.send();
       }
     }
   });
