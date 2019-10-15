32a4200fe9d1a635542ccb600758c3957108b014
Mike Zarra <mkzarra@Mikes-Air.home>
2019-10-15 17:42:12 -0400

    select-multi to multi

--- client/src/components/Items/ItemField.js
+++ client/src/components/Items/ItemField.js
@@ -4,8 +4,7 @@ import Select from 'react-select';
 export default ({ type, options, input, label, meta: { error, touched } }) => {
   console.log('ItemField.js:', input, type);
   let field = <input {...input} type={type} style={{ marginBottom: "5px" }} />;
-  if (type === 'select-multi') field = <Select {...input} onChange={({ value }) => input.onChange(value)} onBlur={() => input.onBlur(input.value)} options={options} />
-
+  if (type === 'select') field = <Select {...input} onChange={({ value }) => input.onChange(value)} onBlur={() => input.onBlur(input.value)} options={options} />
   return (
     <>
       <label>{label}</label>
