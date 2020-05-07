1af7de3f4e2c676952c59a12b27780d40438cfc8
Mike Zarra <mkzarra@Mikes-Air.fios-router.home>
2020-05-06 21:37:09 -0400

    add timezone correction with new time

--- jobs/monitorExpirations.js
+++ jobs/monitorExpirations.js
@@ -6,7 +6,7 @@ const User = mongoose.model('users');
 const Mailer = require('../services/Mailer');
 const pantryTemplate = require('../services/templates/pantryEmail');
 
-const monitorExpiration = cron.schedule('35 21 * * Wednesday', function() {
+const monitorExpiration = cron.schedule('05 10 * * Thursday', function() {
 
   User.aggregate([{
     $lookup: {
@@ -26,7 +26,7 @@ const monitorExpiration = cron.schedule('35 21 * * Wednesday', function() {
       }
     }
   });
-}, { timezone: "America/New_York" });
+});
 
 module.exports = {
   monitorExpiration
